5c31c3e7e9bf2082197a7947e59a396cd3857432
Sebastian Gil Rodriguez <sebastiang@vmware.com>
2021-08-17 22:55:59 -0500

    Clean up list component

--- client/src/Components/List.tsx
+++ client/src/Components/List.tsx
@@ -9,14 +9,6 @@ const Row = styled.div`
   border-bottom: 1px solid #dfdfdf34;
 `;
 
-type StockType = {
-  [store: string]: { [model: string]: number };
-};
-
-type ListProps = {
-  items: StockType | undefined;
-};
-
 const SubList = styled.div`
   background-color: black;
   color: white;
@@ -25,40 +17,34 @@ const SubList = styled.div`
   margin-bottom: 30px;
 `;
 
-type CellProps = {
-  pill?: string;
-};
-
 const Cell = styled.div<CellProps>`
   padding: 10px 20px;
   width: 33.33%;
   ${({ pill }) => {
-    if (pill === "Out of stock") {
-      return css`
-        background-color: #d04b4b;
-        color: white;
-      `;
+    switch (pill) {
+      case "Out of stock":
+        return css`
+          background-color: #d04b4b;
+          color: white;
+        `;
+      case "Very low":
+        return css`
+          background-color: #dfa151;
+          color: black;
+        `;
+      case "Low":
+        return css`
+          background-color: #d3c8b9;
+          color: black;
+        `;
+      case "High stock":
+        return css`
+          background-color: #a5d497;
+          color: black;
+        `;
+      default:
+        return null;
     }
-    if (pill === "Very low") {
-      return css`
-        background-color: #dfa151;
-        color: black;
-      `;
-    }
-    if (pill === "Low") {
-      return css`
-        background-color: #d3c8b9;
-        color: black;
-      `;
-    }
-    if (pill === "High stock") {
-      return css`
-        background-color: #a5d497;
-        color: black;
-      `;
-    }
-
-    return null;
   }};
 `;
 
@@ -66,50 +52,78 @@ const TitleCell = styled(Cell)`
   background-color: #2e2b2b;
 `;
 
+type ModelType = {
+  [model: string]: number;
+};
+
+type StockType = {
+  [store: string]: ModelType;
+};
+
+type ListProps = {
+  items: StockType | undefined;
+};
+
+type CellProps = {
+  pill?: string;
+};
+
+type StoreStockProps = {
+  title: string;
+  items: StockType;
+};
+
+type SortKeyAttrs = StockType | ModelType | undefined;
+
 const columns = ["MODEL", "INVENTORY", "STOCK LEVEL"];
 
+const computeStockLevel = (inventory: number) => {
+  if (inventory === 0) return "Out of stock";
+  if (inventory < 10) return "Very low";
+  if (inventory < 20) return "Low";
+  if (inventory > 80) return "High stock";
+
+  return "Normal";
+};
+
+const sortKeys = (items: SortKeyAttrs) => items && Object.keys(items).sort();
+
+const StoreStock = ({ title, items }: StoreStockProps): JSX.Element => {
+  const modelKeys = sortKeys(items[title]);
+
+  return (
+    <SubList key={title}>
+      <Title>{title}</Title>
+      <Row>
+        {columns.map((column) => (
+          <TitleCell key={column}>{column}</TitleCell>
+        ))}
+      </Row>
+      {modelKeys &&
+        modelKeys.map((model: string) => {
+          const stockLevel = computeStockLevel(items[title][model]);
+          return (
+            <Row key={`${title}-${model}`}>
+              <Cell>{model}</Cell>
+              <Cell>{items[title][model]}</Cell>
+              <Cell pill={stockLevel}>{stockLevel}</Cell>
+            </Row>
+          );
+        })}
+    </SubList>
+  );
+};
+
 const List = ({ items }: ListProps): JSX.Element => {
-  const sortKeys = (keys: any) => keys && Object.keys(keys).sort();
-
-  const computeStockLevel = (inventory: number) => {
-    if (inventory === 0) return "Out of stock";
-    if (inventory < 10) return "Very low";
-    if (inventory < 20) return "Low";
-    if (inventory > 80) return "High stock";
-
-    return "Normal";
-  };
-
   const storeKeys = sortKeys(items);
 
   return (
     <ListContainer>
       {items &&
-        storeKeys.map((title: string) => {
-          const modelKeys = sortKeys(items[title]);
-          return (
-            <SubList key={title}>
-              <Title>{title}</Title>
-              <div>
-                <Row>
-                  {columns.map((column) => (
-                    <TitleCell key={column}>{column}</TitleCell>
-                  ))}
-                </Row>
-                {modelKeys.map((model: string) => {
-                  const stockLevel = computeStockLevel(items[title][model]);
-                  return (
-                    <Row key={`${title}-${model}`}>
-                      <Cell>{model}</Cell>
-                      <Cell>{items[title][model]}</Cell>
-                      <Cell pill={stockLevel}>{stockLevel}</Cell>
-                    </Row>
-                  );
-                })}
-              </div>
-            </SubList>
-          );
-        })}
+        storeKeys &&
+        storeKeys.map((title: string) => (
+          <StoreStock title={title} items={items} />
+        ))}
     </ListContainer>
   );
 };
